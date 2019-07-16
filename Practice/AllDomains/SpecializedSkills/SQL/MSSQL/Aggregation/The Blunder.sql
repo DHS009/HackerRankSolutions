afed3110067d76a421e237bafce81ab726b61e55
Dharmendra Singh <52966345+DHS009@users.noreply.github.com>
2019-07-16 20:01:04 +0530

    New query has been updated
    added new query in the file which is more easy to understand than other queries.

--- Practice/AllDomains/SpecializedSkills/SQL/MSSQL/Aggregation/The Blunder.sql
+++ Practice/AllDomains/SpecializedSkills/SQL/MSSQL/Aggregation/The Blunder.sql
@@ -12,7 +12,10 @@ insert into @EMployees Values (1,'Ashley',2340), (2,'Julia',1198), (3,'Britney',
 (8,'Christeen',2342), (9,'Meera',2343), (10,'Priya',2344), (11,'Priyanka',2345), (12,'Paige',2346), (13,'Jane',2347), (14,'Belvet',2348), (15,'Scarlet',2349), 
 (16,'Salma',9087), (17,'Amanda',7777), (18,'Aamina',5500), (19,'Amina',2570), (20,'Ketty',2007)
 
-select cast(CEILING(avg(cast(salary as float)) - avg(cast(replace(salary,0,'') as float))) as int) from @EMployees
+/* select cast(CEILING(avg(cast(salary as float)) - avg(cast(replace(salary,0,'') as float))) as int) from @EMployees */
+   
+/* This Query is More Understandable */
+select round(avg(EMPLOYEES.SALARY)) - round(avg(replace(salary,0,''))) from EMPLOYEES                                                     
 
 /* Hackerrank accepted solution:
 select cast(CEILING(avg(cast(salary as float)) - avg(cast(replace(salary,0,'') as float))) as int) from employees
